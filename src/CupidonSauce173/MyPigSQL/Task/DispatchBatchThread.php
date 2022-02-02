ac78a34598acd026f4d62b3400eacace0bc7cf6a
CupidonSauce173 <CupidonSauce173@gmail.com>
2022-02-01 23:21:24 -0500

    Update DispatchBatchThread.php

--- src/CupidonSauce173/MyPigSQL/Task/DispatchBatchThread.php
+++ src/CupidonSauce173/MyPigSQL/Task/DispatchBatchThread.php
@@ -32,56 +32,49 @@ class DispatchBatchThread extends Thread
         $nextTime = microtime(true) + 1;
         while ($this->container['runThread']) {
             if (microtime(true) >= $nextTime) {
-                $this->ProcessThread();
+                # Categorize queries into connStrings & creating MySQL connections.
+                /** @var mysqli[] $connections */
+                $connections = [];
+                /** @var SQLRequest $query */
+                foreach ($this->container['batch'] as $query) {
+                    $connString = $query->getConnString();
+                    if (!isset($this->queryContainers[$connString->getName()])) {
+                        $this->queryContainers[$connString->getName()] = [];
+                    }
+                    $conn = new mysqli(
+                        $connString->getAddress(),
+                        $connString->getUsername(),
+                        $connString->getPassword(),
+                        $connString->getDatabase(),
+                        $connString->getPort()
+                    );
+                    $connections[$connString->getName()] = $conn;
+
+                    if (isset($this->queryContainers[$connString->getName()][$query->getId()])) return;
+                    $this->queryContainers[$connString->getName()][$query->getId()] = $query;
+                }
+                # Process all queries
+                foreach ((array)$this->queryContainers as $databaseQueries) {
+                    /** @var SQLRequest $query */
+                    foreach ($databaseQueries as $query) {
+                        if (isset($this->container['executedRequests'][$query->getId()])) return;
+                        $this->container['executedRequests'][] = $query->getId();
+                        $stmt = $connections[$query->getConnString()->getName()]->prepare($query->getQuery());
+                        $stmt->bind_param($query->getDataTypes(), ...$query->getDataKeys());
+                        $stmt->execute();
+                        if ($stmt->error_list) {
+                            throw new SQLRequestException($stmt->error);
+                        }
+                        $data = $stmt->get_result()->fetch_assoc();
+                        $this->container['callbackResults'][$query->getId()] = $data;
+                        $stmt->close();
+                    }
+                }
                 $nextTime = microtime(true) + $this->executionInterval;
             }
         }
     }
 
-    /**
-     * @throws SQLRequestException
-     */
-    private function processThread(): void
-    {
-        # Categorize queries into connStrings & creating MySQL connections.
-        /** @var mysqli[] $connections */
-        $connections = [];
-        /** @var SQLRequest $query */
-        foreach ($this->container['batch'] as $query) {
-            $connString = $query->getConnString();
-            if (!isset($this->queryContainers[$connString->getName()])) {
-                $this->queryContainers[$connString->getName()] = [];
-            }
-            $conn = new mysqli(
-                $connString->getAddress(),
-                $connString->getUsername(),
-                $connString->getPassword(),
-                $connString->getDatabase(),
-                $connString->getPort()
-            );
-            $connections[$connString->getName()] = $conn;
-
-            if (isset($this->queryContainers[$connString->getName()][$query->getId()])) return;
-            $this->queryContainers[$connString->getName()][$query->getId()] = $query;
-        }
-        # Process all queries
-        foreach ((array)$this->queryContainers as $databaseQueries) {
-            /** @var SQLRequest $query */
-            foreach ($databaseQueries as $query) {
-                if (isset($this->container['executedRequests'][$query->getId()])) return;
-                $this->container['executedRequests'][] = $query->getId();
-                $stmt = $connections[$query->getConnString()->getName()]->prepare($query->getQuery());
-                $stmt->bind_param($query->getDataTypes(), ...$query->getDataKeys());
-                $stmt->execute();
-                if ($stmt->error_list) {
-                    throw new SQLRequestException($stmt->error);
-                }
-                $data = $stmt->get_result()->fetch_assoc();
-                $this->container['callbackResults'][$query->getId()] = $data;
-                $stmt->close();
-            }
-        }
-    }
 
     /**
      * @param int $seconds
